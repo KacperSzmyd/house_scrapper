d5b208e1e3515a359d10727d10b737e80d7d9125
Kacper <k.g.szmyd@gmail.com>
2025-02-27 16:48:06 +0100

    Added functions to scrap data from tiles to dict

--- HouseScrapper.py
+++ HouseScrapper.py
@@ -3,6 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import ElementNotInteractableException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 
 class HouseScrapper:
@@ -22,36 +26,40 @@ class HouseScrapper:
                 By.XPATH, value='//*[@id="ckInfoBox"]/div/div/ul/li[2]/span'
             )
             accept_cookies.click()
-            sleep(2)
+            sleep(1)
         except ElementNotInteractableException:
             print("Cookies already accepted")
 
         self.open_dropdown()
-        sleep(2)
         self.choose_houses()
-        sleep(2)
         self.fill_localization()
-        sleep(2)
 
     def open_dropdown(self):
         open_dropdown = self.driver.find_element(
             By.XPATH, value='//*[@id="categoryAliasSingle"]'
         )
         open_dropdown.click()
+        sleep(1)
 
     def choose_houses(self):
         choose_house = self.driver.find_element(
             By.XPATH, value='//*[@id="categoryAliasSingle"]/span[2]/span/span[3]'
         )
         choose_house.click()
+        sleep(1)
 
     def fill_localization(self):
         type_localization = self.driver.find_element(By.ID, value="cityTip")
         type_localization.send_keys(self.localization)
         type_localization.send_keys(Keys.ENTER)
 
-    def get_current_url(self):
+    def get_current_url(self) -> str:
         return self.driver.current_url
+
+    def get_filtered_houses_count(self) -> int:
+        filtered_houses_counter = self.driver.find_element(By.ID, value="boxOfCounter")
+        number_of_houses = filtered_houses_counter.text
+        return int(number_of_houses.split()[0])
 
     def apply_filters(self, min_price=1000000, max_price=2000000):
         filter_menu = self.driver.find_element(
@@ -65,14 +73,59 @@ class HouseScrapper:
         )
         filter_minimum.send_keys(min_price)
         filter_minimum.send_keys(Keys.ENTER)
-        sleep(2)
+        sleep(0.5)
 
         filter_maximum = self.driver.find_element(By.XPATH, value='//*[@id="priceto"]')
         filter_maximum.send_keys(max_price)
         filter_minimum.send_keys(Keys.ENTER)
-        sleep(2)
+        sleep(0.5)
 
         submit_filters = self.driver.find_element(
             By.XPATH, value='//*[@id="search-action-popup-submit"]'
         )
         submit_filters.click()
+
+    def get_house_tiles(self) -> dict:
+        ignored_exceptions = (
+            NoSuchElementException,
+            StaleElementReferenceException,
+        )
+        house_counter = self.get_filtered_houses_count()
+        houses_data = {}
+        sleep(2)
+
+        for x in range(1, house_counter + 1):
+            tile = WebDriverWait(
+                self.driver, 300, ignored_exceptions=ignored_exceptions
+            ).until(
+                expected_conditions.presence_of_element_located(
+                    (By.XPATH, f'//*[@id="tertiary-name_{x}"]')
+                )
+            )
+            tile.click()
+            sleep(2)
+            houses_data[x] = self.get_house_data()
+
+            exit_btn = self.driver.find_element(
+                By.XPATH, value='//*[@id="top-layer-close"]/em'
+            )
+            exit_btn.click()
+
+        return houses_data
+
+    def get_house_data(self) -> dict:
+        house_price = self.driver.find_element(
+            By.XPATH, value='//*[@id="bottomBox"]/p[2]/span[1]'
+        ).text
+        area = self.driver.find_element(
+            By.XPATH, value='//*[@id="bottomBox"]/p[2]/span[2]'
+        ).text
+        price_per_m2 = self.driver.find_element(
+            By.XPATH, value='//*[@id="bottomBox"]/p[2]/span[3]'
+        ).text
+        house_data = {
+            "price": house_price,
+            "area": area,
+            "price per m2": price_per_m2,
+        }
+        return house_data
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import sys
 from HouseScrapper import HouseScrapper
-from bs4 import BeautifulSoup
+
+sys.stdout.reconfigure(encoding="utf-8")
 
 hs = HouseScrapper("Sanok")
 hs.get_houses()
 hs.apply_filters()
-  
+
+houses_data = hs.get_house_tiles()
